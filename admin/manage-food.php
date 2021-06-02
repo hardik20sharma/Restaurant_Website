8dbe1a49cb033ace0ed583979de83bf6512e9e9f
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2021-06-02 16:24:58 +0530

    Video 7

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -4,46 +4,86 @@
         <div class="wrapper">
             <h1>Manage Food</h1>
 
-            <br />
-            <br />
-            <a href="#" class="btn-primary">Add Food</a>
-            <br />
-            <br />
+            <br /><br />
+
+            <?php
+                if(isset($_SESSION['add']))
+                {
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                }
+            ?>
+
+            <br><br>
+
+            <a href="add-food.php" class="btn-primary">Add Food</a>
+            <br /><br />
             
             <table class="tbl-full">
                 <tr>
                     <th>S. No.</th>
-                    <th>FUll Name</th>
-                    <th>UserName</th>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Featured</th>
+                    <th>Active</th>
                     <th>Actions</th>
                 </tr>
-                <tr>
-                    <td>1.</td>
-                    <td>Hardik Sharma</td>
-                    <td>hardik20sharma</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2.</td>
-                    <td>Hardik Sharma</td>
-                    <td>hardik20sharma</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3.</td>
-                    <td>Hardik Sharma</td>
-                    <td>hardik20sharma</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger">Delete Admin</a>
-                    </td>
-                </tr>
+
+                <?php
+                    $sql = "SELECT * FROM tbl_food";
+                    $res = mysqli_query($conn, $sql);
+                    $count = mysqli_num_rows($res);
+
+                    if($count > 0)
+                    {
+                        $sn = 1;
+                        while($row = mysqli_fetch_assoc($res))
+                        {
+                            $id = $row['id'];
+                            $title = $row['title'];
+                            $description = $row['described'];
+                            $price = $row['price'];
+                            $image_name = $row['image_name'];
+                            $featured = $row['featured'];
+                            $active = $row['active'];
+
+                            ?>
+                                <tr>
+                                    <td><?php echo $sn++; ?></td>
+                                    <td><?php echo $title; ?></td>
+                                    <td>
+                                        <?php
+                                            if($image_name == "")
+                                            {
+                                                echo "<div class='error'>Image not added.</div>";
+                                            }
+                                            else
+                                            {
+                                                ?>
+                                                    <img src="<?php echo SITEURL;?>images/food/<?php echo $image_name?>" width='100px'>
+                                                <?php
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $description; ?></td>
+                                    <td><?php echo $price; ?></td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="#" class="btn-secondary">Update Food</a>
+                                        <a href="#" class="btn-danger">Delete Food</a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    }
+                    else
+                    {
+                        echo "<tr><td colspan='7' class='error'>Food not added yet.</td></tr>";
+                    }
+                ?>
             </table>
         </div>
     </div>
